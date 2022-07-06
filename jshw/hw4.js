ea04fe6d12ccba80a710e2735ee6303542c812b4
Sviatoslav <opryshkosm@icloud.com>
2022-07-06 19:51:25 +0300

    fourth hw2

--- jshw/hw4.js
+++ jshw/hw4.js
@@ -1,25 +1,5 @@
 'use strict';
 
-// // Task 1
-// const lowerNumber = +prompt('Write a lower number of a range (from 10 to 99)');
-// const upperNumber = +prompt('Write a upper number of a range (from 10 to 99)');
-
-// let getRangeOfNumbers = function(lower, upper) {
-//     let arr = [];
-//     for (let i = 10; i < 100; i++) {
-//         arr.push(i);
-//     }
-//     const index1 = arr.indexOf(lower);
-//     const index2 = arr.indexOf(upper);
-
-//     return arr.slice(index1, index2 + 1);
-// };
-// // getRangeOfNumbers(lowerNumber, upperNumber);
-
-// function getExactNumber() {
-//     getRangeOfNumbers(lowerNumber, upperNumber).forEach();
-
-// }
 
 //---------------------------------------------------------------------------
 //Test_2 
@@ -28,35 +8,30 @@
 // Вивести в консоль лінію заданої ширини заданим символом
 
 
-// function paintFigure() {
-//     const askWidth = +prompt('Width');
-//     const askSymbol = prompt('Symbol');
+function paintFigure() {
+    const askWidth = +prompt('Width');
+    const askSymbol = prompt('Symbol');
 
-//     if (isNaN(askWidth)) {
-//         +prompt('Uncorrect value of width. Try again');
-//     }
+    if (isNaN(askWidth)) {
+        +prompt('Uncorrect value of width. Try again');
+    }
     
-//     let finalPaint = '';
-//     for (let i = 0; i < askWidth; i++) {
-//         finalPaint += `${askSymbol}`;
-//     }
-//     console.log(finalPaint);
-// }
-// paintFigure();
+    let finalPaint = '';
+    for (let i = 0; i < askWidth; i++) {
+        finalPaint += `${askSymbol}`;
+    }
+    console.log(finalPaint);
+}
+paintFigure();
 
 //---------------------------------------------------------------------------
-//Test_2 
+//Test_3 
 
 function dnaFunc(code) {
-    let a = code.split('').map(change());// ['A', 'T', 'T', 'C', 'G']
+    let a = code.split('');// ['A', 'T', 'T', 'C', 'G']
 
-    return a;
-}
-console.log(dnaFunc('ATTCG'));
-
-function change(code) {
     for (let i = 0; i < code.length; i++) {
-        switch(a) {
+        switch(a[i]) {
             case 'A':
                 a[i] = 'T';
                 break;
@@ -71,4 +46,7 @@ function change(code) {
                 break;
         }  
     }
+    return a;
 }
+console.log(dnaFunc('ATTCG'));
+
