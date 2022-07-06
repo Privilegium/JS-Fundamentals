6e391bd872d24b64d64447442a254b63a7ee61b0
Sviatoslav <opryshkosm@icloud.com>
2022-07-06 15:06:05 +0300

    first gut setting

--- jshw/hw1.js
+++ jshw/hw1.js
@@ -1,11 +1,12 @@
 // Practice
 
-// // Task 1
+// Task 1
 
 let circleArea = prompt('What is the area of the circle?', '');
 alert(`The area of the circle is - ${circleArea}`); 
 
-// // // Task 2 
+//---------------------------------------------------------------------------
+// Task 2
 
 let userPension = prompt('How old are you?', '');
 let yearRemaining = 65 - userPension;
@@ -13,7 +14,8 @@ let pensionYear = 2022 + yearRemaining;
 
 alert(yearRemaining + ', ' + String(pensionYear));
 
-// // Task 3
+//---------------------------------------------------------------------------
+// Task 3
 
 let threeSymbNum = prompt('Write down a three-digit number', '');
 console.log(threeSymbNum.split('').reverse().join(''));
--- jshw/hw2.js
+++ jshw/hw2.js
@@ -1,31 +1,31 @@
 // Task 1
 
-let num = prompt('What\'s your ticket number?')
+let num = prompt('What\'s your ticket number?');
 
 function isLucky(num) {
-    let str = normalNum(num)
+    let str = normalNum(num);
 
-    let sum1 = (+str[0]) + (+str[1]) + (+str[2])
-    let sum2 = (+str[3]) + (+str[4]) + (+str[5])
+    let sum1 = (+str[0]) + (+str[1]) + (+str[2]);
+    let sum2 = (+str[3]) + (+str[4]) + (+str[5]);
 
-    return sum1 === sum2
+    return sum1 === sum2;
 }
 
 function normalNum(num) {
 
     if (num.length == 5) {
-        num = '0' + num
+        num = '0' + num;
     }
     if (num.length == 4) {
-        num = '00' + num
+        num = '00' + num;
     }
-    return num
+    return num;
 }
 
     if (isLucky(num)) {
         alert('Your ticket ' + num + ' is LUCKY!');
     } else {
-        alert('Your ticket is\'t lucky(')
+        alert('Your ticket is\'t lucky(');
     }
 
 
@@ -35,41 +35,41 @@ function normalNum(num) {
 
 
 function askForTemp () {
-    let temp = prompt('Write a Celsius temperature')
-    temp = +temp
+    let temp = prompt('Write a Celsius temperature');
+    temp = +temp;
 
-    return temp
+    return temp;
 }
 
 function convertTemp(temp) {
-    let result = askForTemp()
+    let result = askForTemp();
 
     if (isNaN(result)) {
         console.log('Error');
     } else {
-        result = (result * 1.8) + 32
+        result = (result * 1.8) + 32;
         console.log('It is ' + result + ' in Fahrenheit');
     }
 }
-convertTemp()
+convertTemp();
 
 
 //---------------------------------------------------------------------------
 // Task 4
 
-let l = prompt('What is the length?') 
-let w = prompt('What is the width?')
-let h = prompt('What is the height?') 
+let l = prompt('What is the length?') ;
+let w = prompt('What is the width?');
+let h = prompt('What is the height?');
 
 function getArea () {  
     let officeArea = ((+l * +h) * 2) + ((+w * +h) * 2);
-    return officeArea
+    return officeArea;
 }
 
 function jarNum() {
     let jar = 16; 
 
     let result = getArea() / jar;
-    console.log(result)
+    console.log(result);
 }
 jarNum();
