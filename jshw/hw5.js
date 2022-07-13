805e8c6067fcae81d0772caa6abb9508a076ad8b
Sviatoslav <opryshkosm@icloud.com>
2022-07-13 18:18:00 +0300

    hw55

--- jshw/hw5.js
+++ jshw/hw5.js
@@ -4,128 +4,128 @@
 //Task_5
 
 
-// function sortNum(str) {
-//   let arr = str.split(" ");
-//   let newArr = [];
-//   for (let word of arr) {
-//     word.split("").forEach((letter) => {
-//       if (!isNaN(letter)) {
-//         newArr[letter - 1] = word;
-//       }
-//     });
-//   }
-//   return newArr;
-// }
-// console.log(sortNum("4of Fo1r pe6ople g3ood th5e the2"));
+function sortNum(str) {
+  let arr = str.split(" ");
+  let newArr = [];
+  for (let word of arr) {
+    word.split("").forEach((letter) => {
+      if (!isNaN(letter)) {
+        newArr[letter - 1] = word;
+      }
+    });
+  }
+  return newArr;
+}
+console.log(sortNum("4of Fo1r pe6ople g3ood th5e the2"));
 
 
 //---------------------------------------------------------------------------
 //TASK_6 
 
 
-// const check = [
-//   {
-//     Назва: "Мівіна",
-//     Кількість: 3,
-//     Вартість: 10,
-//   },
-//   {
-//     Назва: "Йогурт",
-//     Кількість: 5,
-//     Вартість: 25,
-//   },
-//   {
-//     Назва: "Кокакола",
-//     Кількість: 2,
-//     Вартість: 15,
-//   },
-// ];
+const check = [
+  {
+    Назва: "Мівіна",
+    Кількість: 3,
+    Вартість: 10,
+  },
+  {
+    Назва: "Йогурт",
+    Кількість: 5,
+    Вартість: 25,
+  },
+  {
+    Назва: "Кокакола",
+    Кількість: 2,
+    Вартість: 15,
+  },
+];
 
-// function logCheck(arr) {
-//   let checkLog = `Ваші покупки:\n\n`;
+function logCheck(arr) {
+  let checkLog = `Ваші покупки:\n\n`;
 
-//   for (let object of arr) {
-//     for (let key in object) {
-//       switch (key) {
-//         case "Назва":
-//           checkLog += `${key}: ${object[key]}\n`;
-//           break;
-//         case "Кількість":
-//           checkLog += `${key}: ${object[key]} шт.\n`;
-//           break;
-//         case "Вартість":
-//           checkLog += `${key}: ${object[key]}грн. за шт.\n\n`;
-//           break;
-//       }
-//     }
-//   }
+  for (let object of arr) {
+    for (let key in object) {
+      switch (key) {
+        case "Назва":
+          checkLog += `${key}: ${object[key]}\n`;
+          break;
+        case "Кількість":
+          checkLog += `${key}: ${object[key]} шт.\n`;
+          break;
+        case "Вартість":
+          checkLog += `${key}: ${object[key]}грн. за шт.\n\n`;
+          break;
+      }
+    }
+  }
 
-//   return checkLog;
-// }
+  return checkLog;
+}
 
-// console.log(logCheck(check));
+console.log(logCheck(check));
 
-// function checkSum(arr) {
-//   let cash = 0;
+function checkSum(arr) {
+  let cash = 0;
 
-//   arr.forEach((a) => {
-//     cash += a["Кількість"] * a["Вартість"];
-//   });
+  arr.forEach((a) => {
+    cash += a["Кількість"] * a["Вартість"];
+  });
 
-//   return `Сумарна вартість покупки: ${cash} грн.`;
-// }
+  return `Сумарна вартість покупки: ${cash} грн.`;
+}
 
-// console.log(checkSum(check));
+console.log(checkSum(check));
 
-// function showExp(arr) {
-//   let allSum = [];
+function showExp(arr) {
+  let allSum = [];
 
-//   arr.forEach((a) => {
-//     allSum.push(a["Кількість"] * a["Вартість"]);
-//     allSum.sort((a, b) => a - b);
-//   }); //[30, 30, 125]
+  arr.forEach((a) => {
+    allSum.push(a["Кількість"] * a["Вартість"]);
+    allSum.sort((a, b) => a - b);
+  }); //[30, 30, 125]
 
-//   let mostExp = ``;
-//   arr.forEach((a) => {
-//     const math = a["Кількість"] * a["Вартість"];
-//     if (math === allSum[allSum.length - 1]) {
-//       mostExp += `Найдорожча покупка в чеку - ${a["Назва"]}, сума  - ${math}`;
-//     }
-//   });
-//   return mostExp;
-// }
+  let mostExp = ``;
+  arr.forEach((a) => {
+    const math = a["Кількість"] * a["Вартість"];
+    if (math === allSum[allSum.length - 1]) {
+      mostExp += `Найдорожча покупка в чеку - ${a["Назва"]}, сума  - ${math}`;
+    }
+  });
+  return mostExp;
+}
 
-// console.log(showExp(check));
+console.log(showExp(check));
 
-// function getAveragePrice(arr) {
-//   let sumAmount = 0;
-//   let sumPrice = 0;
+function getAveragePrice(arr) {
+  let sumAmount = 0;
+  let sumPrice = 0;
 
-//   arr.forEach((a) => {
-//     sumPrice += a["Кількість"] * a["Вартість"];
-//     sumAmount += a["Кількість"];
-//   });
+  arr.forEach((a) => {
+    sumPrice += a["Кількість"] * a["Вартість"];
+    sumAmount += a["Кількість"];
+  });
 
-//   let averagePrice = sumPrice / sumAmount;
-//   return `Середня ціна одного товару дорівнює ${Math.floor(
-//     averagePrice
-//   )} грн. та ${(averagePrice % 2) * 100} коп.`;
-// }
-// console.log(getAveragePrice(check));
+  let averagePrice = sumPrice / sumAmount;
+  return `Середня ціна одного товару дорівнює ${Math.floor(
+    averagePrice
+  )} грн. та ${(averagePrice % 2) * 100} коп.`;
+}
+console.log(getAveragePrice(check));
 
 
 //---------------------------------------------------------------------------
 //TASK_7 
 
 
-// let defence = 1;
+let defence = 1;
 
-// function howMuchHeads() {
-//     prompt('Write how much head have each dragon(with coma\'s and space)?').split(', ').forEach(a => defence *= a);
-//     return defence;
-// }
+function howMuchHeads() {
+    prompt('Write how much head have each dragon(with coma\'s and space)?').split(', ').forEach(a => defence *= a);
+    return defence;
+}
 
-// console.log(howMuchHeads());
+console.log(howMuchHeads());
 
 
 //---------------------------------------------------------------------------
@@ -150,22 +150,49 @@ function checkBoard(arr) {
         draw: 0
     };
 
-    let b1 = 0;
-    let b2 = 0;
-    let b3 = 0;
+    const verticalMatch = {
+        b1: 0,
+        b2: 0,
+        b3: 0
+    };
 
     for (let array of arr) { //! Ми беремо окремі array
+        let sum = 0;
         for (let value of array) { //! Ми беремо окремі значення
-            let sum = 0;
             sum += value;
 
-            if (sum === 6) {
-                return answers.winO;
-            } else if (sum === 3) {
-                return answers.winX;
+            if (sum === 6) {                        // todo Перевірка горизонтальна
+                return answers.winO;                // todo Перевірка горизонтальна
+            } else if (sum === 2 && value !== 1) {  // todo Перевірка горизонтальна
+                break;                              // todo Перевірка горизонтальна
+            } else if (sum === 3) {                 // todo Перевірка горизонтальна
+                return answers.winX;                // todo Перевірка горизонтальна
             }
         }
+                
+        // for (let i = 0; i < 3; i++) {
+        //     switch (i) {
+        //         case 0:
+        //             verticalMatch.b1 += array[i];
+        //             break;
+        //         case 1:
+        //             verticalMatch.b2 += array[i];
+        //             break;
+        //         case 2:
+        //             verticalMatch.b3 += array[i];
+        //             break;
+        //     }
+        // }
 
+        // for (let vertical in verticalMatch) {
+        //     if (vertical === 6) {                   // todo Перевірка горизонтальна
+        //         return answers.winO;                // todo Перевірка горизонтальна
+        //     } else if (vertical === 2 && value !== 1) {  // todo Перевірка горизонтальна
+        //         break;                              // todo Перевірка горизонтальна
+        //     } else if (vertical === 3) {                 // todo Перевірка горизонтальна
+        //         return answers.winX;                // todo Перевірка горизонтальна
+        //     }
+        // }
     }    
 }
 
